6c5722d9493831dc332037fe3626bd0fc5c87bf7
nolan <nolanghiggins@gmail.com>
2021-11-27 15:43:46 -0700

    added comments so when i forget how to do it in the future

--- factorial.js
+++ factorial.js
@@ -1,10 +1,10 @@
 function factorial(n) {
-  let result = n;
+  let result = n; //assigns the input "n" to result
 
-  if (n === 0 || n === 1) return 1;
+  if (n === 0 || n === 1) return 1; //returns 1 if n is equal to 0 or 1
   while (n > 1) {
-    n--;
-    result = result * n;
+    n--; //decrements n by 1 with each iteration
+    result = result * n; //re-assigns the variable result to equal result * n
   }
   return result;
 }
