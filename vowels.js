20d0f6b9934eb2c3908b7e6d862a31efc08f6b5f
nolan <nolanghiggins@gmail.com>
2021-11-28 19:19:51 -0700

    used the for(of) loop to log all vowels and consonants

--- vowels.js
+++ vowels.js
@@ -1,31 +1,10 @@
-const vowels = ["a", "e", "i", "o", "u"]; //put in cap vowels
+const vowels = ["a", "e", "i", "o", "u"];
 
 function vowelsAndConsonants(s) {
-  for (let i = 0; i < s.length; i++) {
-    if (i === vowels.includes(i)) {
-      console.log("i");
-    } else i === !vowels.includes(i);
-    console.log(i);
-  }
-}
-
-/* const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
-
-function vowelsAndConsonants(s) {
-  let string = s;
-  let stringArray = string.split("");
-  console.log(stringArray);
-} */
-
-/* 
-function vowelsAndConsonants(s) {
-  let vowels = ["a", "e", "i", "o", "u"];
-
   for (let v of s) {
     if (vowels.includes(v)) console.log(v);
   }
-
   for (let v of s) {
     if (!vowels.includes(v)) console.log(v);
   }
-}*/
+}
