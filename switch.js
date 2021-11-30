b5ef781f2dc2a6ea63b21dfac7732a6428d5ed4a
nolan <nolanghiggins@gmail.com>
2021-11-30 15:39:01 -0700

    didnt use any switch statements because i hate them

--- switch.js
+++ switch.js
@@ -1,4 +1,30 @@
-function getLetter(s){
-    let letter = ;
-    return letter;
+const letterA = ["a", "e", "i", "o", "u"];
+const letterB = ["b", "c", "d", "e", "f", "g"];
+const letterC = ["h", "i", "j", "k", "l", "m"];
+const letterD = [
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+function getLetter(s) {
+  let L = s.charAt[0];
+
+  if (letterA.includes(L)) {
+    return "A";
+  } else if (letterB.includes(L)) {
+    return "B";
+  } else if (letterC.includes(L)) {
+    return "C";
+  } else return "D";
 }
