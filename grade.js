a179f0d108d755c15b94e0af8e46a5a0356cd8af
nolan <nolanghiggins@gmail.com>
2021-11-29 18:55:40 -0700

    im not proud of this one

--- grade.js
+++ grade.js
@@ -1,15 +1,30 @@
+const grade = ["A", "B", "C", "D", "E", "F"];
+
 function getGrade(score) {
-  let grade = ["A", "B", "C", "D", "E", "F"];
-  if (score > 25 && score <= 30) {
-    console.log(grade[0]);
-  } else if (score > 20 && score <= 25) {
-    console.log(grade[1]);
-  } else if (score > 15 && score <= 20) {
-    console.log(grade[2]);
-  } else if (score > 10 && score <= 15) {
-    console.log(grade[3]);
-  } else if (score > 5 && score <= 10) {
-    console.log(grade[4]);
-  } else score > 0 && score <= 5;
-  console.log(grade[5]);
+  let a = 25 < score <= 30;
+  let b = 20 < score <= 25;
+  let c = 15 < score <= 20;
+  let d = 10 < score <= 15;
+  let e = 5 < score <= 10;
+  let f = 0 < score <= 5;
+  switch (true) {
+    case a:
+      console.log(grade[0]);
+      break;
+    case b:
+      console.log(grade[0]);
+      break;
+    case c:
+      console.log(grade[0]);
+      break;
+    case d:
+      console.log(grade[0]);
+      break;
+    case e:
+      console.log(grade[0]);
+      break;
+    case e:
+      console.log(grade[0]);
+      break;
+  }
 }
