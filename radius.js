c9486165f98970c71cdcedb9233943380dc6f98b
nolan <nolanghiggins@gmail.com>
2021-11-29 17:12:39 -0700

    re structured file

--- radius.js
+++ radius.js
@@ -3,11 +3,10 @@
 // given r print area then on a new line print perimeter
 
 const PI = Math.PI;
-
 const r = readLine();
 
-let area = PI * r * r;
-console.log(parseFloat(area));
-
+let area = PI * r ** 2;
 let per = 2 * PI * r;
+
+console.log(parseFloat(area));
 console.log(parseFloat(per));
