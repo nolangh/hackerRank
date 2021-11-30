45e522aefcd536ae918dd70bcd21e8bf4c9fe844
nolan <nolanghiggins@gmail.com>
2021-11-29 18:14:14 -0700

    i have no idea why i am struggling with this

--- grade.js
+++ grade.js
@@ -1,10 +1,15 @@
 function getGrade(score) {
   let grade = ["A", "B", "C", "D", "E", "F"];
-  if (score > 25 || score <= 30) return grade[0];
-  else if (score > 20 || score <= 25) return grade[1];
-  else if (score > 15 || score <= 20) return grade[2];
-  else if (score > 10 || score <= 15) return grade[3];
-  else if (score > 5 || score <= 10) return grade[4];
-  else score > 0 || score <= 5;
-  return grade[5];
+  if (score > 25 && score <= 30) {
+    console.log(grade[0]);
+  } else if (score > 20 && score <= 25) {
+    console.log(grade[1]);
+  } else if (score > 15 && score <= 20) {
+    console.log(grade[2]);
+  } else if (score > 10 && score <= 15) {
+    console.log(grade[3]);
+  } else if (score > 5 && score <= 10) {
+    console.log(grade[4]);
+  } else score > 0 && score <= 5;
+  console.log(grade[5]);
 }
