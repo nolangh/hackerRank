7e16950789b086f161759f486549001e0ada7c38
nolan <nolanghiggins@gmail.com>
2021-11-29 17:10:10 -0700

    returns area followed by perimeter

--- radius.js
+++ radius.js
@@ -1,1 +1,13 @@
+// area = PI* radius
+// perimeter = 2 * PI * r
+// given r print area then on a new line print perimeter
+
 const PI = Math.PI;
+
+const r = readLine();
+
+let area = PI * r * r;
+console.log(parseFloat(area));
+
+let per = 2 * PI * r;
+console.log(parseFloat(per));
