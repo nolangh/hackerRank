1dbd51f58c54f42f5de9bcf320749e086e6981d2
nolan <nolanghiggins@gmail.com>
2021-12-02 15:04:41 -0700

    filtered then sorted array and returned second highest value

--- array.js
+++ array.js
@@ -1,10 +1,14 @@
 //itterate through array
 //compare each element
+
 function getSecondLargest(nums) {
-  let sortedArray = nums.sort((a, b) => a - b);
-  for (let v of sortedArray) {
-    if (v - v === 1) {
-      console.log(v);
-    }
-  }
+  let cleanedArray = nums.filter(
+    (value, index) => nums.indexOf(value) === index
+  );
+  let sortedArray = cleanedArray.sort((a, b) => a - b);
+  return sortedArray[1];
 }
+
+/* function getSecondLargest(nums) {
+  let sortedArray = nums.sort((a, b) => a - b);
+} */
