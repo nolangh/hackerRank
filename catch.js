2832624c129a3cdf8b5c358998c06cf8c8061a40
nolan <nolanghiggins@gmail.com>
2021-12-03 22:28:36 -0700

    made some comments about what needs to be done

--- catch.js
+++ catch.js
@@ -1,1 +1,5 @@
+//Try to reverse string  using the split, reverse, and join methods.
+// if an exception is thrown, catch it and print the contents of the exceptions "message" on a new line.
+//print s on a new line. if no exception was thrown, then this should be the reversed string; if an exception was thrown, this should be the original string.
+
 function reverseString(s) {}
