4cefb56d290db75b018849c026181eb42ec25eb3
nolan <nolanghiggins@gmail.com>
2021-12-05 16:38:07 -0700

    put in temp error catching function

--- catch.js
+++ catch.js
@@ -2,8 +2,15 @@
 // if an exception is thrown, catch it and print the contents of the exceptions "message" on a new line.
 //print s on a new line. if no exception was thrown, then this should be the reversed string; if an exception was thrown, this should be the original string.
 
-function reverseString(s) {
-    reverse string here 
-    catch exceptions here
-    print message here
+function getElement(arr, pos) {
+  return arr[pos];
 }
+
+//let arr = [1, 2, 3, 4, 5];
+
+try {
+  console.log(getElement(arr, 4));
+} catch (e) {
+  console.log(e.message);
+}
+console.log("The program continued executing!");
