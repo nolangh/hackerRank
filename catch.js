a8c5b434c656824c032a8a9e7ea5b23cf87c0396
nolan <nolanghiggins@gmail.com>
2021-12-05 18:06:30 -0700

    reversed the string. now i need to return the correct message

--- catch.js
+++ catch.js
@@ -1,12 +1,15 @@
-//Try to reverse string  using the split, reverse, and join methods.
+//Try to reverse string using the split, reverse, and join methods.
 // if an exception is thrown, catch it and print the contents of the exceptions "message" on a new line.
 //print s on a new line. if no exception was thrown, then this should be the reversed string; if an exception was thrown, this should be the original string.
 
 function reverseString(s) {
   let string = s.split("");
+  let stringArr = string.reverse().join("");
 
   try {
-    console.log(string.reverse);
+    console.log(stringArr);
+  } catch (err) {
+    console.log(err.message);
   } finally {
     console.log(s);
   }
